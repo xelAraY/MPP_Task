36c5a0bcd4880578d50ae28087850c6920e720ed
xelAraY <99496200+xelAraY@users.noreply.github.com>
2023-04-06 03:45:22 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default class App extends React.Component {
   };
 
   ResetDate = () => {
-    this.setState({ date: null });
+    this.setState({ date: '' });
   };
 
   ChangeDate = (date) => {
